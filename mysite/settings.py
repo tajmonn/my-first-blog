80384edf15ce3471ba8b1ef232d4a1ef3cf9c875
tajmonn <fuatita@gmail.com>
2020-04-02 20:06:50 +0200

    jprd

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pe@y0^f@t#%^-10cz8=y=b$w#bp9wu8^5fl81n)p=c$tnnkdgk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
