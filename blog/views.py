51f536efb9f86474ab4ee238ef0791b81d6b1005
tajmonn <fuatita@gmail.com>
2020-04-02 19:34:58 +0200

    Zmieniony kod HTML na stronieeee

--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     
-def post_detail(request, pk):
-    Post.objects.get(pk=pk)
+Post.objects.get(pk=pk)
     
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
