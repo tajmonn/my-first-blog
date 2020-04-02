f995ccd4031662043ce49012219d1f9427240ceb
tajmonn <fuatita@gmail.com>
2020-04-02 19:44:55 +0200

    jprd

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,6 @@ def post_list(request):
 
     
 def post_detail(request, pk):
-    Post.object.get(pk=pk)
+    
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
